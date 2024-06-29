fb4c7d12fc015a1b892c6ccf63d9e9033a99fc5e
KNyathi <khayelihlenyathi07@gmail.com>
2024-06-29 15:08:48 +0300

    Adding scraping file, and corresponding changes

--- Backend/backend/backend/settings.py
+++ Backend/backend/backend/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'Nyathik123',
-        'HOST': 'localhost',
+        'HOST': 'db',
         'PORT': '5432',
     }
 }
--- Frontend/frontend/src/App.js
+++ Frontend/frontend/src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Applicants from './components/Applicants';
 import Vacancies from './components/Vacancies';
 
 function App() {
     return (
         <Router>
-            <Switch>
-                <Route path="/applicants" component={Applicants} />
-                <Route path="/vacancies" component={Vacancies} />
-            </Switch>
+            <Routes>
+                <Route path="/applicants" element={<Applicants />} />
+                <Route path="/vacancies" element={<Vacancies />} />
+            </Routes>
         </Router>
     );
 }
